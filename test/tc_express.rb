a39d5ad3196367eec6a64d526b4e0e969e64d6b6
Roben Kleene <contact@robenkleene.com>
2019-12-05 19:52:48 -0500

    Add test express default (#11)

--- test/tc_express.rb
+++ test/tc_express.rb
@@ -3,8 +3,8 @@
 require 'minitest/autorun'
 require_relative 'lib/test_setup'
 
-# Test server
-class TestServer < Minitest::Test
+# Test express
+class TestExpress < Minitest::Test
   EXTERNAL_DIRECTORY = File.expand_path(
     File.join(__dir__, '../external/repla-test-express/')
   )
@@ -13,6 +13,40 @@ class TestServer < Minitest::Test
   def setup
     Dir.chdir(EXTERNAL_DIRECTORY) do
       `#{SERVER_BUNDLE_COMMAND} -p 3000 "#{EXTERNAL_COMMAND}"`
+    end
+    window_id = nil
+    Repla::Test.block_until do
+      window_id = Repla::Test::Helper.window_id
+      !window_id.nil?
+    end
+    refute_nil(window_id)
+    @window = Repla::Window.new(window_id)
+  end
+
+  def teardown
+    @window.close
+  end
+
+  def test_express
+    javascript = File.read(Repla::Test::TITLE_JAVASCRIPT_FILE)
+    result = nil
+    Repla::Test.block_until do
+      result = @window.do_javascript(javascript)
+      result == HTML_TITLE
+    end
+    assert_equal(HTML_TITLE, result)
+  end
+end
+
+class TestExpressDefault < Minitest::Test
+  EXTERNAL_DIRECTORY = File.expand_path(
+    File.join(__dir__, '../external/repla-test-express/')
+  )
+  EXTERNAL_COMMAND = 'DEBUG=myapp:* npm start'.freeze
+  HTML_TITLE = 'Express'.freeze
+  def setup
+    Dir.chdir(EXTERNAL_DIRECTORY) do
+      `#{SERVER_BUNDLE_COMMAND} "#{EXTERNAL_COMMAND}"`
     end
     window_id = nil
     Repla::Test.block_until do
